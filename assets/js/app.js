21cff26e7cf6690cb347b2c457eb282fb15c1125
Nicholas Scheurich <nick@scheurich.me>
2020-04-24 21:58:52 -0500

    Add Phoenix LiveView

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,16 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-import css from "../css/app.css"
+import { Socket } from "phoenix";
+import LiveSocket from "phoenix_live_view";
+import NProgress from "nprogress";
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
-import "phoenix_html"
+import "../css/app.css";
+import "phoenix_html";
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+const liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } });
+
+liveSocket.connect();
+
+window.liveSocket = liveSocket;
+
+window.addEventListener("phx:page-loading-start", () => NProgress.start());
+window.addEventListener("phx:page-loading-stop", () => NProgress.done());
