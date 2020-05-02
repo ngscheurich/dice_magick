7ee1ba3c81ff1b8beedc0f5b292958171e62097d
Nicholas Scheurich <nick@scheurich.me>
2020-05-01 23:41:00 -0500

    Simplify
    This removes stats and archetypes. While I liked the idea, I decided
    that the complex UI necessary for these features is out of scope for the
    initial version of this project. Moving forward, roll creation will be
    handled by parsing CSV data (that was likely generated outside of the
    app).

--- lib/dice_magick/archetypes/archetype.ex
+++
@@ -1,9 +0,0 @@
-defmodule DiceMagick.Archetype do
-  @moduledoc """
-  Defines a type of character derived from a particular TTRPG system.
-  """
-
-  @callback name() :: atom()
-  @callback encode(map()) :: {:ok, map()} | {:error, keyword()}
-  # @callback decode(map()) :: {:ok, map()} | {:error, keyword()}
-end
