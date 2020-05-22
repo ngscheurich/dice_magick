ea48d2dc09510fa1c487bd209b28ab26423e2a96
Nicholas Scheurich <nick@scheurich.me>
2020-05-22 18:30:00 -0500

    Remove unused aliases

--- lib/rolls.ex
+++ lib/rolls.ex
@@ -3,37 +3,8 @@ defmodule Rolls do
   [todo] Write documentation
   """
 
-  alias Rolls.{Roll, Encoders}
+  alias Rolls.Roll
   alias Repo
-
-  @doc """
-  Translates the given `data` into a list of `Roll`s.
-
-  ## Options
-
-  * `format` - Specifies the format of the incoming data. Defaults to `:csv`.
-
-  ## Examples
-
-      iex> encode_rolls(~s(Cursed Greatsword,"1d10 - 1"))
-      %Roll{name: "Cursed Greatsword", parts: [{1, 10, -1}]}
-
-      iex> encode_rolls(~s(Wisdom Save,"1d20, 4")
-      {:error, "Error message"}
-
-  """
-  @spec encode_rolls(any(), format: atom()) :: Sources.Source.parsed()
-  def encode_rolls(data, opts \\ []) do
-    module =
-      opts
-      |> Keyword.get(:format, :csv)
-      |> module_for_format()
-
-    module.encode(data)
-  end
-
-  @spec module_for_format(SourceEnum.t()) :: atom()
-  defp module_for_format(:csv), do: Encoders.CSV
 
   @doc """
   Creates a `Roll`.
@@ -47,7 +18,7 @@ defmodule Rolls do
       {:error, %Ecto.Changeset{}}
 
   """
-  @spec create_roll(map()) :: {:ok, Roll.t()} | {:error, Changeset.t()}
+  @spec create_roll(map) :: {:ok, Roll.t()} | {:error, Changeset.t()}
   def create_roll(attrs \\ %{}) do
     %Roll{}
     |> Roll.changeset(attrs)
