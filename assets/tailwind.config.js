1fecabaa8f3d12abdb396776083cb8b9e0533eac
Nicholas Scheurich <nick@scheurich.me>
2020-05-25 17:15:57 -0500

    Start working on custom UI

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,22 +1,48 @@
 module.exports = {
   purge: [],
   theme: {
+    colors: {
+      "primary-100": "#EFE8E8",
+      "primary-200": "#BDA4A4",
+      "primary-300": "#9D7676",
+      "primary-400": "#7C4949",
+      "primary-500": "#5B1B1B",
+      "primary-600": "#491616",
+      "primary-700": "#371010",
+      "primary-800": "#240B0B",
+      "primary-900": "#120505",
+
+      "secondary-100": "#F0EAE6",
+      "secondary-200": "#C3AD99",
+      "secondary-300": "#A48366",
+      "secondary-400": "#865A33",
+      "secondary-500": "#683100	",
+      "secondary-600": "#532700",
+      "secondary-700": "#3E1D00",
+      "secondary-800": "#2A1400",
+      "secondary-900": "#150A00",
+
+      "neutral-100": "#F9F5F3",
+      "neutral-200": "#E7D7CF",
+      "neutral-300": "#DBC4B7",
+      "neutral-400": "#CFB09F",
+      "neutral-500": "#C39C87",
+      "neutral-600": "#9C7D6C",
+      "neutral-700": "#755E51",
+      "neutral-800": "#4E3E36",
+      "neutral-900": "#271F1B",
+
+      "dark-100": "#0E0B0B",
+      "dark-200": "#0F0C0C",
+      "dark-300": "#272424",
+
+      transparent: "transparent",
+
+      highlight: "#E39C5E",
+    },
     fontFamily: {
       serif: ["Grenze", "serif"],
       mono: ["IBM Plex Mono", "monospace"],
-    },
-    extend: {
-      colors: {
-        dark: "#100C0C",
-        "dark-red": "#320000",
-        "light-red": "#5B1B1B",
-        // orange: "#683100",
-        brown: "#2A211B",
-        // yellow: "#E39C5E",
-        "dark-tan": "#C39C87",
-        "light-tan": "#CDBBA8",
-        light: "#E4D8CD",
-      },
     },
   },
   variants: {},
