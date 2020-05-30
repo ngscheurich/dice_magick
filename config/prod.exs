a5446cf7f63b904c8f5d1c238332f913fa509cc1
Nicholas Scheurich <nick@scheurich.me>
2020-05-30 14:55:23 -0500

    More deploy configuration

--- config/prod.exs
+++ config/prod.exs
@@ -10,11 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dice_magick, Web.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "www.dicemagick.app", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+# config :logger, level: :info
 
 # ## SSL Support
 #
