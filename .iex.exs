653900aed87f7a1506a0c4278fef8554b5620496
Nicholas Scheurich <nick@scheurich.me>
2020-06-01 18:14:58 -0500

    Configure Logger level

--- .iex.exs
+++ .iex.exs
@@ -10,5 +10,4 @@ alias Rolls.{Roll, Encoder, CSVEncoder}
 alias Taxonomy.{Tag, RollTag}
 alias Sources.{GoogleSheets, Test, Source}
 
-character_id = "c0a02bd8-2c75-4ea7-bf8a-d0037c1a758a"
-character = Characters.get_character!(character_id)
+Logger.configure(level: :info)
