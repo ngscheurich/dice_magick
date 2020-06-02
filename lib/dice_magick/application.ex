bc2e633bc53a3c49df71a7cdf31079d4c332b317
Nicholas Scheurich <nick@scheurich.me>
2020-06-02 18:31:32 -0500

    Start character workers on application boot

--- lib/dice_magick/application.ex
+++ lib/dice_magick/application.ex
@@ -5,6 +5,21 @@ defmodule DiceMagick.Application do
 
   use Application
 
+  defmodule CharacterWorkers do
+    use GenServer
+
+    def start_link(_), do: GenServer.start_link(__MODULE__, :ok)
+
+    @impl true
+    def init(:ok) do
+      Characters.Character
+      |> Repo.all()
+      |> Enum.each(&Characters.Supervisor.add_worker(&1.id))
+
+      :ignore
+    end
+  end
+
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
@@ -22,8 +37,14 @@ defmodule DiceMagick.Application do
         start: {Characters.Supervisor, :start_link, [[]]}
       },
       # Start the Discord consumer
-      Discord
+      Discord,
     ]
+
+    children =
+      case Mix.env() do
+        :test -> children
+        _ -> children ++ [CharacterWorkers]
+      end
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
