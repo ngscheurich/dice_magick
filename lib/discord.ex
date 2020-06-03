b53c8176b8f7fa7c0292ca4e0142579774a393b9
Nicholas Scheurich <nick@scheurich.me>
2020-06-02 20:36:50 -0500

    Delegate to roll command module

--- lib/discord.ex
+++ lib/discord.ex
@@ -17,41 +17,7 @@ defmodule Discord do
   end
 
   def handle_event({:MESSAGE_CREATE, %{content: "!dm " <> input} = msg, _ws_state}) do
-    id = to_string(msg.author.id)
-
-    character =
-      User
-      |> where(discord_uid: ^id)
-      |> limit(1)
-      |> Repo.one!()
-      |> Repo.preload(:characters)
-      |> Map.get(:characters)
-      |> List.first()
-
-    rolls =
-      character.id
-      |> Characters.Worker.state()
-      |> Map.get(:rolls)
-      |> Enum.filter(fn roll ->
-        name = String.downcase(roll.name)
-        input = String.downcase(input)
-        String.starts_with?(name, input)
-      end)
-
-    message =
-      case rolls do
-        [roll] ->
-          result = ExDiceRoller.roll(roll.expression)
-
-          """
-          **#{character.name}** rolls _#{roll.name}_ (`#{roll.expression}`)…
-          :game-die: Result: **#{result}**
-          """
-
-        _ ->
-          "Sorry, I couldn’t find a roll matching “#{input}”."
-      end
-
+    {_, message} = Commands.Roll.process([input], msg)
     Api.create_message(msg.channel_id, message)
   end
 
