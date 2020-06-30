f105cd01afb8023d3221fbfa0e534b9b0c8eeb9a
Nicholas Scheurich <nick@scheurich.me>
2020-06-30 18:48:15 -0500

    Get GCP credentials from the system environment

--- config/config.exs
+++ config/config.exs
@@ -40,6 +40,8 @@ config :nostrum,
 config :logger,
   backends: [:console, Sentry.LoggerBackend]
 
+config :goth, json: System.get_env("GCP_CREDENTIALS")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
